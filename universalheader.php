44c4899f2c13a943fcd53ab8a46662604f8c387c
BrainyBrian <dgramopadhye@gmail.com>
2015-02-11 15:55:07 -0500

    Create universalheader.php

---
+++ universalheader.php
@@ -0,0 +1,17 @@
+<!-- Note that you can type html in a php file, it will render as normal, only difference is that you can have php in here too-->
+<!--Remember to NEVER change the ID or CLASS or NAME or any of those type of params in any html, it may cause other code to fail. There are several ways around this. Ask in wiki.-->
+<html>
+  <head>
+    <!--META tags and Favicon-->
+    <!--No more META tags and Favicon-->
+    <script src="jsforunivheader.js"></script><!--Keep as much JS in that file as possible - this is to prevent clutter-->
+    <style src="stylesforunivhearder.css"></style><!--Keep as much CSS in that file as possible - this is to prevent clutter-->
+  </head>
+  <body>
+    <header>
+      <?php include 'universalheader.php';?><!--TODO: Alternatively put iframe if it dosnt work.-->
+    </header>
+    <footer>
+    </footer>
+  </body>
+</html>
