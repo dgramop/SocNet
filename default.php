005b20c292127581e0758137d392da496454f316
BrainyBrian <dgramopadhye@gmail.com>
2015-02-10 21:12:46 -0500

    Update default.php

--- default.php
+++ default.php
@@ -1,1 +1,16 @@
 <!-- Note that you can type html in a php file, it will render as normal, only difference is that you can have php in here too-->
+<!--Remember to NEVER change the ID or CLASS or NAME or any of those type of params in any html, it may cause other code to fail. There are several ways around this. Ask in wiki.-->
+<html>
+  <head>
+    <!--META tags and Favicon-->
+    <!--No more META tags and Favicon-->
+    <script src="jsforindex.js"></script><!--Keep as much JS in that file as possible - this is to prevent clutter-->
+  </head>
+  <body>
+    <header>
+    <div id="MenuHolder">
+    </header>
+    <footer>
+    </footer>
+  </body>
+</html>
